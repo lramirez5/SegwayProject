4f7b6383aa9556890be3e7f407b1f68fafd70dce
lgu33 <45438217+ZEIPBR743@users.noreply.github.com>
2018-12-09 16:04:09 -0600

    Update Segway_tb.sv

--- Segway_tb.sv
+++ Segway_tb.sv
@@ -36,6 +36,10 @@ SegwayModel iPHYS(.clk(clk),.RST_n(RST_n),.SS_n(SS_n),.SCLK(SCLK),
   around SPI_ADC128S.sv that mimics the behavior of the A2D converter on the DE0 used
   to read ld_cell_lft, ld_cell_rght and battery
 */  
+ADC128S iA2D(.clk(clk),.rst_n(RST_n),//.ld_cell_lft(ld_cell_lft),.ld_cell_rght(ld_cell_rght),//inputs
+			.SS_n(SS_A2D_n),.SCLK(SCLK_A2D_n),
+			.MOSI(MOSI_A2D_n),//inputs
+			.MISO(MISO_A2D_n));
   
 ////// Instantiate DUT ////////
 Segway iDUT(.clk(clk),.RST_n(RST_n),.LED(),.INERT_SS_n(SS_n),.INERT_MOSI(MOSI),
@@ -53,12 +57,41 @@ UART_tx iTX(.clk(clk),.rst_n(RST_n),.TX(RX_TX),.trmt(send_cmd),.tx_data(cmd),.tx
 
 
 initial begin
-  Initialize;		// perhaps you make a task that initializes everything?  
+  //TODO initialize;		// perhaps you make a task that initializes everything?  
   ////// Start issuing commands to DUT //////
- 
-  repeat(50000) @(posedge clk);
+  //initialize (RST_n,clk, send_cmd,SS_n , MOSI, PWM_frwrd_rght, PWM_rev_rght,
+  //     PWM_frwrd_lft, PWM_rev_lft, rider_lean,MISO , A2D_MISO,INT);
+  rider_lean = 14'b0;	
+  clk = 1'b0;
+  RST_n = 1'b0;
+  send_cmd = 1'b0;
+  repeat (50) @(posedge clk);
+  RST_n = 1'b1;
+			
+					// Serial in from inertial sensor
+		
+
+
+
   
-  SendCmd(8'h67);	// perhaps you have a task that sends 'g'
+  //repeat(50000) @(posedge clk);
+  
+  //SendCmd(8'h67);	// perhaps you have a task that sends 'g' TODO
+  cmd = 8'h67;
+  send_cmd = 1'b1;
+  rider_lean = 14'b0;
+  
+  //ewpwRCRO
+  repeat(1000000)
+  
+    @(posedge clk);
+    rider_lean = 14'h3FF;
+     
+    repeat(1000000)
+    @(posedge clk);
+    rider_lean = 14'h001;
+  
+  $stop;
 /*
     .
 	.	// this is the "guts" of your test
@@ -75,3 +108,9 @@ always
 //`include "tb_tasks.v"	// perhaps you have a separate included file that has handy tasks.
 
 endmodule	
+
+//begins task that sends
+//module SendCmd(snd_cmd, signal); 
+  //snd_cmd = 1'b1;
+  //cmd = signal;
+//endmodule
